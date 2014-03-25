b8145507ef12809616f114d1702289678d6c0bba
robochuck84 <trogers84@gmail.com>
2014-03-25 17:16:23 -0400

    about half way through, much faster with emacs on yourside

--- toys.scm
+++ toys.scm
@@ -14,8 +14,8 @@
             (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
             
 (define add1
-    (lambda (n)
-        (+ n 1)))
+     (lambda (n)
+       (+ n 1)))
 
 (define sub1
     (lambda (n)
@@ -35,3 +35,66 @@
             (else
                 (cons (add (car tup1) (car tup2)) 
                     (tup+ (cdr tup1) (cdr tup2)))))))
+(define gt
+  (lambda (n m)
+    (cond
+     ((zero? n) #f)
+     ((zero? m) #t)
+     (else (gt (sub1 n) (sub1 m))))))
+
+(define lt
+  (lambda (n m)
+    (cond
+     ((zero? m) #f)
+     ((zero? n) #t)
+     (else (lt (sub1 n) (sub1 m))))))
+
+
+
+(define equals
+  (lambda (n m)
+    (cond
+     ((gt n m) #f)
+     ((lt n m) #f)
+     (else #t))))
+
+(define pow
+  (lambda (n m)
+    (cond
+     ((zero? m) 1)
+     (else (* n (pow n (sub1 m)))))))
+
+(define sub
+  (lambda (n m)
+    (cond
+     ((zero? m) n)
+     (else (sub1 (sub n (sub1 m)))))))
+
+(define divide
+  (lambda (n m)
+    (cond
+     ((lt n m) 0)
+     (else (add1 (divide (sub n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+     ((null? lat) 0)
+     (else (+ 1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+     ((zero? n) (car lat))
+     (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((zero? n) (cdr lat))
+     (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+     ((number? (car lat))))))
